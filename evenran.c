afd52b3c58245533c3d343f68db58648bb4f25f8
Shivang <34168289+Shi-vang@users.noreply.github.com>
2018-05-03 17:10:19 +0530

    Add files via upload

---
+++ evenran.c
@@ -0,0 +1,14 @@
+#include<stdio.h>  
+int main()
+{    
+	int i,j,a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	for(i=a+1;i<
+	b;i++){
+		if(i%2==0){
+			printf("%d ",i);
+		}
+	}
+	return 0;  
+ }    
---
+++ fact.c
@@ -0,0 +1,14 @@
+#include<stdio.h>  
+int main()
+{    
+	int i,j,a,b=1;
+	scanf("%d",&a);
+	if(a>0){
+	for(i=1;i<=a;i++){
+		b=b*i;
+		
+	}
+}
+	printf("%d",b);
+	return 0;  
+ }    
